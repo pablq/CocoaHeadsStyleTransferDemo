6108ea1202f9e0ed8dcaec7dbe9ba3b16a472ec0
Pablo Philipps <pabloignaciophilipps@gmail.com>
2021-08-15 18:49:26 -0500

    Initial Commit

---
+++ CocoaHeadsStyleTransferDemo/CocoaHeadsStyleTransferDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaHeadsStyleTransferDemoApp.swift
+//  CocoaHeadsStyleTransferDemo
+//
+//  Created by Pablo Philipps on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CocoaHeadsStyleTransferDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
